6588b89339535271a44a87c4d186b01411858691
slein-nl <sleino321@gmail.com>
2024-03-03 21:07:52 +0100

    refactor

--- nav.c
+++ nav.c
@@ -1,4 +1,3 @@
-#include <ncurses.h>
 #include <signal.h>
 #include <curses.h>
 #include <time.h>
@@ -24,16 +23,17 @@ struct entry_array {
     char* entries;
 };
 
-struct found_ptrs { 
+struct entry_ptrs { 
     int max_size;
     int dir_count;
     int file_count;
-    char** found_ptrs;
+    char** ptrs;
 };
 
-struct entry_array* file_array;
-struct entry_array* dir_array;
-struct found_ptrs* found_array; 
+struct entry_array file_array;
+struct entry_array dir_array;
+struct entry_ptrs found_ptrs; 
+struct entry_ptrs all_ptrs; 
 char current_path[PATH_MAX];
 int current_path_length;
 char* user_shell;
@@ -57,37 +57,34 @@ void init()
     user_shell = getenv("SHELL");
     getcwd(current_path, PATH_MAX);
     current_path_length = strlen(current_path);
-
-    file_array = (struct entry_array*)malloc(sizeof(struct entry_array));
-    dir_array = (struct entry_array*)malloc(sizeof(struct entry_array));
-    found_array = (struct found_ptrs*)malloc(sizeof(struct found_ptrs));
-
-    if (file_array == NULL || dir_array == NULL || found_array == NULL) {
-        panic("memory allocation error");
-    }
     
-    file_array->entry_count = 0;
-    file_array->max_size = 512;
-    file_array->max_ptrs_size = 512;
-    file_array->entries_size = 0;
-    file_array->entries = (char*)malloc(512);
-    file_array->entry_pointers = (char**)malloc(512);
-    dir_array->entry_count = 0;
-    dir_array->max_size = 512;
-    dir_array->max_ptrs_size = 512;
-    dir_array->entries_size = 0;
-    dir_array->entries = (char*)malloc(512);
-    dir_array->entry_pointers = (char**)malloc(512);
-    found_array->dir_count = 0;
-    found_array->file_count= 0;
-    found_array->found_ptrs = malloc(512);
-    found_array->max_size = 512;
+    file_array.entry_count = 0;
+    file_array.max_size = 512;
+    file_array.max_ptrs_size = 512;
+    file_array.entries_size = 0;
+    file_array.entries = (char*)malloc(512);
+    file_array.entry_pointers = (char**)malloc(512);
+    dir_array.entry_count = 0;
+    dir_array.max_size = 512;
+    dir_array.max_ptrs_size = 512;
+    dir_array.entries_size = 0;
+    dir_array.entries = (char*)malloc(512);
+    dir_array.entry_pointers = (char**)malloc(512);
+    found_ptrs.dir_count = 0;
+    found_ptrs.file_count= 0;
+    found_ptrs.ptrs = malloc(512);
+    found_ptrs.max_size = 512;
+    all_ptrs.dir_count = 0;
+    all_ptrs.file_count= 0;
+    all_ptrs.ptrs = malloc(512);
+    all_ptrs.max_size = 512;
 
-    if (file_array->entries == NULL ||
-        dir_array->entries == NULL ||
-        dir_array->entry_pointers == NULL ||
-        file_array->entry_pointers == NULL ||
-        found_array->found_ptrs == NULL) {
+    if (file_array.entries == NULL ||
+        dir_array.entries == NULL ||
+        dir_array.entry_pointers == NULL ||
+        file_array.entry_pointers == NULL ||
+        found_ptrs.ptrs == NULL ||
+        all_ptrs.ptrs == NULL) {
 
         panic("memory allocation error");
     }
@@ -130,12 +127,12 @@ void extend_entry_array_pointers(struct entry_array* arr)
     }
 }
 
-void extend_found_array()
+void extend_ptrs_array(struct entry_ptrs* arr)
 {
-    char** realloced = (char**)realloc(found_array->found_ptrs, found_array->max_size * 2);
+    char** realloced = (char**)realloc(arr->ptrs, arr->max_size * 2);
     if (realloced) {
-        found_array->found_ptrs = realloced;
-        found_array->max_size *= 2;
+        arr->ptrs = realloced;
+        arr->max_size *= 2;
     }
     else {
         panic("realloc error");
@@ -144,11 +141,11 @@ void extend_found_array()
 
 void add_found_ptr(char* ptr)
 {
-    int current_array_size = sizeof(char*) * (found_array->dir_count + found_array->file_count);
-    if (current_array_size + sizeof(char*) > found_array->max_size) {
-        extend_found_array();
+    int current_array_size = sizeof(char*) * (found_ptrs.dir_count + found_ptrs.file_count);
+    if (current_array_size + sizeof(char*) > found_ptrs.max_size) {
+        extend_ptrs_array(&found_ptrs);
     }
-    char** array_head = (char**)((char*)found_array->found_ptrs + current_array_size);
+    char** array_head = (char**)((char*)found_ptrs.ptrs + current_array_size);
     *array_head = ptr;
 }
 
@@ -183,22 +180,35 @@ void get_dir_contents(char* dirname)
 
     // ent = readdir(dir); // the "." directory is omitted
 
-    dir_array->entry_count = 0;
-    file_array->entry_count = 0;
-    found_array->file_count = 0;
-    found_array->dir_count = 0;
+    dir_array.entry_count = 0;
+    file_array.entry_count = 0;
+    found_ptrs.file_count = 0;
+    found_ptrs.dir_count = 0;
     
     while ((ent = readdir(dir)) != NULL) {
         if (ent->d_type == DT_DIR) {
-            add_entry(ent->d_name, dir_array);
+            add_entry(ent->d_name, &dir_array);
         } 
         else {
-            add_entry(ent->d_name, file_array);
+            add_entry(ent->d_name, &file_array);
         }
     }
 
-    qsort(dir_array->entry_pointers, dir_array->entry_count, sizeof(char*), compare_entries);
-    qsort(file_array->entry_pointers, file_array->entry_count, sizeof(char*), compare_entries);
+    qsort(dir_array.entry_pointers, dir_array.entry_count, sizeof(char*), compare_entries);
+    qsort(file_array.entry_pointers, file_array.entry_count, sizeof(char*), compare_entries);
+    
+    uint64_t all_size = (file_array.entry_count + dir_array.entry_count) * sizeof(char**);
+    if (all_ptrs.max_size < all_size) {
+        char** realloced = realloc(all_ptrs.ptrs, all_size);
+        if (realloced) {
+            all_ptrs.ptrs = realloced;
+        }
+        all_ptrs.max_size = all_size;
+    }
+    memcpy(all_ptrs.ptrs, dir_array.entry_pointers, dir_array.entry_count * sizeof(char**));
+    memcpy(all_ptrs.ptrs + dir_array.entry_count, file_array.entry_pointers, file_array.entry_count * sizeof(char*));
+    all_ptrs.file_count = file_array.entry_count;
+    all_ptrs.dir_count = dir_array.entry_count;
 }
 
 WINDOW* make_window() 
@@ -212,7 +222,7 @@ WINDOW* make_window()
     return window;
 }
 
-void draw_found_entries(uint32_t selected_index)
+void draw_found_entries(uint32_t selected_index, struct entry_ptrs* ptrs)
 {
     int x = 0;
     int y = 3;
@@ -221,12 +231,12 @@ void draw_found_entries(uint32_t selected_index)
         column_count = 1;
     }
     int entries_per_row = 0;
-    for (int i = 0; i < found_array->dir_count; i++) {
+    for (int i = 0; i < ptrs->dir_count; i++) {
         int color = 2;
         if (selected_index == i) color--;
         wattron(win, COLOR_PAIR(color));
-        mvwprintw(win, y, x, "%.*s", longest_entry, found_array->found_ptrs[i]);
-        int len = strlen(found_array->found_ptrs[i]);
+        mvwprintw(win, y, x, "%.*s", longest_entry, ptrs->ptrs[i]);
+        int len = strlen(ptrs->ptrs[i]);
         if (len > MAX_ENTRY_LENGTH) {
             mvwprintw(win, y, x + longest_entry - 3, "...");
         }
@@ -240,12 +250,12 @@ void draw_found_entries(uint32_t selected_index)
             entries_per_row = 0;
         }
     }
-    for (int i = 0; i < found_array->file_count; i++) {
+    for (int i = ptrs->dir_count; i < ptrs->file_count + ptrs->dir_count; i++) {
         int color = 2;
-        if (selected_index == i + found_array->dir_count) color--;
+        if (selected_index == i ) color--;
         wattron(win, COLOR_PAIR(color));
-        mvwprintw(win, y, x, "%.*s", longest_entry, found_array->found_ptrs[i]);
-        int len = strlen(found_array->found_ptrs[i]);
+        mvwprintw(win, y, x, "%.*s", longest_entry, ptrs->ptrs[i]);
+        int len = strlen(ptrs->ptrs[i]);
         if (len > MAX_ENTRY_LENGTH) {
             mvwprintw(win, y, x + longest_entry - 3, "...");
         }
@@ -270,12 +280,12 @@ void draw_entries(uint32_t selected_index)
         column_count = 1;
     }
     int entries_per_row = 0;
-    for (int i = 0; i < dir_array->entry_count; i++) {
+    for (int i = 0; i < dir_array.entry_count; i++) {
         int color = 2;
         if (selected_index == i) color--;
         wattron(win, COLOR_PAIR(color));
-        mvwprintw(win, y, x, "%.*s", longest_entry, dir_array->entry_pointers[i]);
-        int len = strlen(dir_array->entry_pointers[i]);
+        mvwprintw(win, y, x, "%.*s", longest_entry, dir_array.entry_pointers[i]);
+        int len = strlen(dir_array.entry_pointers[i]);
         if (len > MAX_ENTRY_LENGTH) {
             mvwprintw(win, y, x + longest_entry - 3, "...");
         }
@@ -289,12 +299,12 @@ void draw_entries(uint32_t selected_index)
             entries_per_row = 0;
         }
     }
-    for (int i = 0; i < file_array->entry_count; i++) {
+    for (int i = 0; i < file_array.entry_count; i++) {
         int color = 2;
-        if (selected_index == i + dir_array->entry_count) color--;
+        if (selected_index == i + dir_array.entry_count) color--;
         wattron(win, COLOR_PAIR(color));
-        mvwprintw(win, y, x, "%.*s", longest_entry, file_array->entry_pointers[i]);
-        int len = strlen(file_array->entry_pointers[i]);
+        mvwprintw(win, y, x, "%.*s", longest_entry, file_array.entry_pointers[i]);
+        int len = strlen(file_array.entry_pointers[i]);
         if (len > MAX_ENTRY_LENGTH) {
             mvwprintw(win, y, x + longest_entry - 3, "...");
         }
@@ -315,24 +325,26 @@ void draw_entries(uint32_t selected_index)
 
 void search_entries(wchar_t* searchstring)
 {
+    found_ptrs.file_count = 0;
+    found_ptrs.dir_count = 0;
     static char multi_byte[NAME_MAX] = {};
     int written = wcstombs(multi_byte, searchstring, NAME_MAX);
     if (written == -1)
         panic("wcstombs error");
     multi_byte[written] = '\0';
 
-    for (int i = 0; i < dir_array->entry_count; i++) {
-        char* ptr = strcasestr(dir_array->entry_pointers[i], multi_byte);
+    for (int i = 0; i < dir_array.entry_count; i++) {
+        char* ptr = strcasestr(dir_array.entry_pointers[i], multi_byte);
         if (ptr) {
-            add_found_ptr(dir_array->entry_pointers[i]);
-            found_array->dir_count++;
+            add_found_ptr(dir_array.entry_pointers[i]);
+            found_ptrs.dir_count++;
         }
     }
-    for (int i = 0; i < file_array->entry_count; i++) {
-        char* ptr = strcasestr(file_array->entry_pointers[i], multi_byte);
+    for (int i = 0; i < file_array.entry_count; i++) {
+        char* ptr = strcasestr(file_array.entry_pointers[i], multi_byte);
         if (ptr) {
-            add_found_ptr(file_array->entry_pointers[i]);
-            found_array->file_count++;
+            add_found_ptr(file_array.entry_pointers[i]);
+            found_ptrs.file_count++;
         }
     }
 }
@@ -350,13 +362,13 @@ void change_directory(char* dir)
 
 void reset_found_array()
 {
-    while ((dir_array->entry_count * sizeof(char**)) + (file_array->entry_count * sizeof(char**)) > found_array->max_size) {
-        extend_found_array();
+    while ((dir_array.entry_count * sizeof(char**)) + (file_array.entry_count * sizeof(char**)) > found_ptrs.max_size) {
+        extend_ptrs_array(&found_ptrs);
     }
-    memcpy(found_array->found_ptrs, dir_array->entry_pointers, dir_array->entry_count * sizeof(char**));
-    memcpy(found_array->found_ptrs + dir_array->entry_count, file_array->entry_pointers, file_array->entry_count * sizeof(char*));
-    found_array->file_count = file_array->entry_count;
-    found_array->dir_count = dir_array->entry_count;
+    memcpy(found_ptrs.ptrs, dir_array.entry_pointers, dir_array.entry_count * sizeof(char**));
+    memcpy(found_ptrs.ptrs + dir_array.entry_count, file_array.entry_pointers, file_array.entry_count * sizeof(char*));
+    found_ptrs.file_count = file_array.entry_count;
+    found_ptrs.dir_count = dir_array.entry_count;
 }
 
 void entry_search_loop() 
@@ -365,9 +377,8 @@ void entry_search_loop()
     wchar_t searchstring[NAME_MAX] = {};
     uint32_t searchstringindex = 0;
     uint32_t selected_index = 0;
-
-    reset_found_array();
-
+    struct entry_ptrs* current_ptrs = &all_ptrs;
+    
     while (true) {
         clock_t start_time = clock();
         get_wch((wint_t*)&c);
@@ -385,8 +396,8 @@ void entry_search_loop()
             wresize(win, winy, winx);
         }
         else if (c == L'\n') {
-            if (selected_index < found_array->dir_count) {
-                change_directory(found_array->found_ptrs[selected_index]);
+            if (selected_index < current_ptrs->dir_count) {
+                change_directory(current_ptrs->ptrs[selected_index]);
                 searchstringindex = 0;
                 searchstring[0] = '\0';
             }
@@ -395,7 +406,7 @@ void entry_search_loop()
             }
         }
         else if (c == L'\t') {
-            if (selected_index != found_array->dir_count + found_array->file_count - 1)
+            if (selected_index != current_ptrs->dir_count + current_ptrs->file_count - 1)
                 selected_index++;
             else
                 selected_index = 0;
@@ -404,7 +415,7 @@ void entry_search_loop()
             if (selected_index != 0) 
                 selected_index--;
             else
-                selected_index = found_array->dir_count + found_array->file_count - 1;
+                selected_index = current_ptrs->dir_count + current_ptrs->file_count - 1;
         }
         else if (c == KEY_BACKSPACE || c == 127 || c == '\b') {
             if (searchstringindex > 0) {
@@ -423,18 +434,16 @@ void entry_search_loop()
         werase(win);
         mvwaddwstr(win, 0, 0, searchstring);
 
-        found_array->file_count = 0;
-        found_array->dir_count = 0;
-
         if (searchstringindex != 0) {
+            current_ptrs = &found_ptrs;
             search_entries(searchstring);
-            if (selected_index > found_array->dir_count + found_array->file_count)
+            if (selected_index > found_ptrs.dir_count + found_ptrs.file_count)
                 selected_index = 0;
-            draw_found_entries(selected_index);
+            draw_found_entries(selected_index, current_ptrs);
         }
         else {
-            draw_entries(selected_index);  
-            reset_found_array();
+            current_ptrs = &all_ptrs;
+            draw_found_entries(selected_index, current_ptrs);  
         } 
         
         wmove(win, 0, searchstringindex);
